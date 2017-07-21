2ab5373585d6da554675429dd0b1b20cfb35bfff
doktor_justice <somiandras@gmail.com>
2017-07-21 19:41:32 +0200

    List of data errors to be corrected

---
+++ cleaning.py
@@ -0,0 +1,7 @@
+# Issues to clean before upload:
+# 1. Missing 'utca' from 'Kucsma' in one case
+# 2. Lower-case street names
+# 3. Remove 'H-' from postcode
+# 4. Change 1503 and 1507 postcodes to 1053 and 1057
+
+
