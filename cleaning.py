7f97573e3ccf7399eb1bd4ad7f14330786654614
doktor_justice <somiandras@gmail.com>
2017-07-22 09:10:15 +0200

    Clean street names and postcodes before JSON dump

--- cleaning.py
+++ cleaning.py
@@ -5,12 +5,8 @@ import re
 import lxml.etree as ET
 import pprint as pp
 import json
+from datetime import datetime
 
-# Issues to clean before upload:
-# 1. Missing 'utca' from 'Kucsma' in one case
-# 2. Lower-case street names
-# 3. Remove 'H-' from postcodes
-# 4. Change 1503 and 1507 postcodes to 1053 and 1057
 
 filename = 'budapest_hungary_inner.osm'
 TAGS_TO_PROCESS = ['node', 'way', 'relation']
@@ -59,16 +55,55 @@ def shape_element(element):
         return None
 
 
+def clean_streetname(streetname):
+    if streetname == 'Kucsma':
+        return 'Kucsma utca'
+    elif streetname.lower() == streetname:
+        streetname[0] = streetname[0].upper()
+        return streetname
+    else:
+        return streetname
+
+
+def clean_postcode(postcode):
+    postcode_string = str(postcode)
+    if postcode_string[:2] == 'H-':
+        return int(postcode_string[2:])
+    elif postcode_string == '1503':
+        return 1053
+    elif postcode_string == '1507':
+        return 1057
+    else:
+        return int(postcode)
+
+
+def correct_data_entry(elem_data):
+    # Issues to clean before upload:
+    # 1. Missing 'utca' from 'Kucsma' in one case
+    # 2. Lower-case street names
+    # 3. Remove 'H-' from postcodes
+    # 4. Change 1503 and 1507 postcodes to 1053 and 1057
+    # 5. Change timestamp to datetime
+
+    if 'address' in elem_data:
+        if 'street' in elem_data['address']:
+            elem_data['address']['street'] = clean_streetname(elem_data['address']['street'])
+        if 'postcode' in elem_data['address']:
+            elem_data['address']['postcode'] = clean_postcode(elem_data['address']['postcode'])
+    return elem_data
+
+
 def process_file():
     data = []
     counter = 0
     for event, elem in ET.iterparse(filename, events=('start',)):
+        counter += 1
+        print(counter)
         elem_data = shape_element(elem)
         if elem_data:
-            data.append(elem_data)
-        # counter += 1
-        # if counter == 10000:
-        #     break
+            data.append(correct_data_entry(elem_data))
+        if counter == 10000:
+            break
     return data
 
 
